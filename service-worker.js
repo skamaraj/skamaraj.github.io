09909a09e92dfb0d50d68560fce1496781bc875e
Snigdha <snigdhadevelops@gmail.com>
2017-06-08 07:28:27 -0400

    Network Requests

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,46 @@
+const cachePrefix = 'mission-03';
+
+function fetchFromCache (request) {
+  return caches.match(request).then(response => {
+    if (!response) {
+      throw Error(`${request.url} not found in cache`);
+    }
+    return response;
+  });
+}
+
+function addToCache (request, response) {
+  if (response.ok) {
+    const copy = response.clone();
+    caches.open(`${cachePrefix}-assets`).then(cache => {
+      cache.put(request, copy);
+    });
+  }
+  return response;
+}
+
+function offlinePage () {
+  return caches.open(`${cachePrefix}-offline`).then(cache => {
+    return cache.match('offline.html');
+  });
+}
+
+function isNavigateRequest (request) {
+  return (request.mode === 'navigate' ||
+     (request.method === 'GET' &&
+       request.headers.get('accept').includes('text/html')));
+}
+
+function isImageRequest (request) {
+  return (request.headers.get('Accept').indexOf('image') !== -1);
+}
+
 self.addEventListener('install', event => {
-  var offlineURL = 'offline.html';
+  const offlineURL = 'offline.html';
   event.waitUntil(
     fetch(new Request(offlineURL)).then(response => {
-      return caches.open('offline').then(cache => {
-        return cache.put(new Request(offlineURL), response);
+      return caches.open(`${cachePrefix}-offline`).then(cache => {
+        return cache.put(offlineURL, response);
       });
     })
   );
@@ -11,15 +48,19 @@ self.addEventListener('install', event => {
 
 self.addEventListener('fetch', event => {
   var request = event.request;
-  if (request.mode === 'navigate' ||
-     (request.method === 'GET' &&
-       request.headers.get('accept').includes('text/html'))) {
+  if (isNavigateRequest(request)) {
     event.respondWith(
-      fetch(request).catch(error => {
-        return caches.open('offline').then(cache => {
-          return cache.match('offline.html');
-        });
-      })
+      fetch(request)
+        .then(response => addToCache(request, response))
+        .catch(() => fetchFromCache(request))
+        .catch(() => offlinePage())
+    );
+  } else if (isImageRequest(request)) {
+    event.respondWith(
+      fetchFromCache(request)
+        .catch(() => fetch(request)
+          .then(response => addToCache(request, response)))
+        .catch(() => console.log('unable to respond to request'))
     );
   }
 });
