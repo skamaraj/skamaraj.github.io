091756954cdfa928766040fae6799f02e5af35de
Snigdha <snigdhadevelops@gmail.com>
2017-06-08 04:09:19 -0400

    Service workers experimentation 2

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,24 @@
+self.addEventListener('install', event => {
+  var offlineURL = 'offline.html';
+  event.waitUntil(
+    fetch(new Request(offlineURL)).then(response => {
+      return caches.open('offline').then(cache => {
+        return cache.put(new Request(offlineURL), response);
+      });
+    })
+  );
+});
+
 self.addEventListener('fetch', event => {
   var request = event.request;
   if (request.mode === 'navigate' ||
-    (request.method === 'GET' &&
-     request.headers.get('accept').includes('text/html'))) {
+     (request.method === 'GET' &&
+       request.headers.get('accept').includes('text/html'))) {
     event.respondWith(
       fetch(request).catch(error => {
-        return new Response('<p>Oh, dear.</p>',
-          { headers: { 'Content-Type': 'text/html' } });
+        return caches.open('offline').then(cache => {
+          return cache.match('offline.html');
+        });
       })
     );
   }
